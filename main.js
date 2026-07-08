dd0b08910ef1d6246a20d83e64113d7c2f3574a3
nanara <nomanovaara@gmail.com>
2026-07-08 19:55:23 +0600

    commit

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
-const brgBtn = document.querySelector(".burger-btn");
+console.log("JS работает");
 
-brgBtn.onclick = () => {
-    const brgMenu = document.querySelector(".burger");
-    brgMenu.classList.toggle("active");
-};
+    const cards = document.querySelector(".cards");
+
+fetch("http://localhost:3000/products")
+    .then(res => res.json())
+    .then(data => {
+
+        const product = data[0];
+
+        cards.innerHTML = `
+            <div class="card">
+                <img src="${product.image}" alt="">
+                <h3>${product.title}</h3>
+                <p>${product.price}</p>
+            </div>
+        `;
+    });
+    
